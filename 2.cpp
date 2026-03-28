4b05ca329501976da71e94084a1f21e99860a626
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-03-29 01:37:30 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -35,3 +35,54 @@ int main(){
     traverse(head);      //haed=newnode as head was updated in insertion function
     return 0;
 }
+
+//Insert at the end
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+
+    }
+};
+void insert_at_beginning(node* &head, int data){
+    node* newnode=new node(data);
+    newnode->next=head;
+    head=newnode;
+}
+void insert_at_end(node*&head, int data){
+    node*newnode=new node(data);
+    if(head==NULL){
+        head=newnode;
+        return;
+    }
+    node*temp=head;
+    while(temp->next!=NULL){                 //moves the temp till it reaches last element then add newnode
+        temp=temp->next;
+    }
+    temp->next=newnode;              //make changes in original linkedlist
+}
+void traverse(node*head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+int main(){
+    node * node1=new node(1);
+    node * node2=new node(2);
+    node1->next=node2;
+    node *head=node1;
+    traverse(head);
+    cout<<endl;
+    insert_at_beginning(head,0);
+    insert_at_end(head,3);
+    traverse(head);
+    return 0;
+}
