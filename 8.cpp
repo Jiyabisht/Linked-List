d2a8604ff8badc5482d9b3ec670793eb7624f74d
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-04-08 01:34:04 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -29,3 +29,33 @@ public:
 // | ------------------ | ---- | ----- | ------ |
 // | Counting method    | O(n) | O(1)  | 2      |
 // | Slow-Fast pointers | O(n) | O(1)  | 1 ⭐    |
+
+//Fast and Slow approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+};
+
+// | Complexity Type | Value    |
+// | --------------- | -------- |
+// | Time            | **O(n)** |
+// | Space           | **O(1)** |
